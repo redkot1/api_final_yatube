968da220480d6a47ecb7d5b581af8e08d0aaae0d
Linar <grifon@dalem.ru>
2021-05-22 17:02:12 +0300

    PEP8

--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -5,8 +5,16 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 urlpatterns = [
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        'api/v1/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'api/v1/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path(
